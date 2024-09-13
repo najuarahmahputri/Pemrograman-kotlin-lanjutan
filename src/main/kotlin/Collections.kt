35fae679758df62bf1142dbb5e3571b75d2e770c
WELCOME RAP & NRP <najuarahmahputri@gmail.com>
2024-09-14 02:57:06 +0700

    Collections.kt

---
+++ src/main/kotlin/Collections.kt
@@ -0,0 +1,11 @@
+fun printAll(string: Collection<String>){
+    for (s in string) print("$s")
+    println()
+}
+fun main(args: Array<String>) {
+    val stringList = listOf("one", "two" , "one")
+    printAll(stringList)
+
+    val stringSet = setOf("one", "two" , "three")
+    printAll(stringSet)
+}
